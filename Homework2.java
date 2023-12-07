30a029f8610f611d313ebd46fd4f394a58b36546
dimon <dimon20.07@mail.ru>
2023-12-07 14:14:57 +0300

    .\Homework2.java

---
+++ Homework2.java
@@ -0,0 +1,21 @@
+package org.example;
+
+
+import java.util.ArrayList;
+
+public class Homework2 {
+    private final ArrayList<Developer> developers = new ArrayList<>();
+    public void homework2() {
+
+        developers.add(new FrontEnder());
+        developers.add(new BackEnder());
+        developers.add(new FullStack());
+        developers.add(new FrontEnder());
+
+        for (int i = 0; i < developers.size(); i++) {
+            if (developers.get(i) instanceof FrontEnder) {
+                (developers.get(i)).developGUI();
+            }
+        }
+    }
+}
